da42f8c3216dd0c140f21ff42c7e040672be22af
Robin Green <robin@designingreen.co.uk>
2023-04-11 09:32:11 +0100

    update month and countdown

--- js/countdown.js
+++ js/countdown.js
@@ -4,7 +4,7 @@
         hour = minute * 60,
         day = hour * 24;
 
-  let roundone = "April 7, 2023 15:59:00",
+  let roundone = "April 21, 2023 15:59:00",
       countDown = new Date(roundone).getTime(),
       x = setInterval(function() {    
 
